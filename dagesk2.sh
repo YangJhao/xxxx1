120c3e05d400ec25c46719dab788d8c79e8f32a5
YangJhao <33487713+YangJhao@users.noreply.github.com>
2026-02-09 01:56:29 +0800

    Add files via upload

---
+++ dagesk2.sh
@@ -0,0 +1,45 @@
+
+#!/bin/bash
+yum -y install wget
+cd /root
+wget -c https://github.com/YangJhao/xxxx1/raw/main/3proxy-0.9.4-2.el7.x86_64.rpm
+rpm -ivh /root/3proxy-0.9.4-2.el7.x86_64.rpm
+sleep 2
+mkdir /etc/3proxy
+touch /etc/3proxy/passwd
+echo 'user1:CL:23333' >> /etc/3proxy/passwd
+systemctl enable 3proxy
+# 1. 放行TCP端口50001
+firewall-cmd --permanent --add-port=50001/tcp
+
+# 2. 放行TCP端口60001
+firewall-cmd --permanent --add-port=60001/tcp
+
+# 3.放行UDP端口范围
+firewall-cmd --permanent --add-port=10001-65000/udp
+
+
+# 4. 重新加载firewalld配置使更改生效
+firewall-cmd --reload
+cat /dev/null > /etc/3proxy.cfg
+echo 'daemon' >> /etc/3proxy.cfg
+echo 'timeouts 1 5 30 60 180 1800 15 60' >> /etc/3proxy.cfg
+echo 'nserver 8.8.8.8' >> /etc/3proxy.cfg
+echo 'nserver 223.5.5.5' >> /etc/3proxy.cfg
+echo 'nscache 65536' >> /etc/3proxy.cfg
+echo 'config /etc/3proxy.cfg' >> /etc/3proxy.cfg
+echo 'monitor /etc/3proxy.cfg' >> /etc/3proxy.cfg
+echo 'monitor /etc/3proxy/passwd' >> /etc/3proxy.cfg
+echo 'users $/etc/3proxy/passwd' >> /etc/3proxy.cfg
+echo 'log /var/log/3proxy/3proxy.log D' >> /etc/3proxy.cfg
+echo 'logformat "L%Y-%m-%d %H:%M:%S.%. %N.%p %E %U %C:%c %R:%r %O %I %h %T"' >> /etc/3proxy.cfg
+echo 'archiver gz /bin/gzip %F' >> /etc/3proxy.cfg
+echo 'rotate 90' >> /etc/3proxy.cfg
+echo 'maxconn 100000' >> /etc/3proxy.cfg
+echo 'auth strong' >> /etc/3proxy.cfg
+echo 'allow *' >> /etc/3proxy.cfg
+echo 'proxy -a -p50001' >> /etc/3proxy.cfg
+echo 'socks -a -p60001' >> /etc/3proxy.cfg
+echo 'flush' >> /etc/3proxy.cfg
+sleep 2
+reboot
